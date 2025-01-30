a998b57a423697ac52f2fa29b2060bb8b8a719d2
Dev_Faruq <olatoyesefaruq@gmail.com>
2025-01-30 07:46:48 +0100

    add more project

--- src/components/project.js
+++ src/components/project.js
@@ -24,6 +24,13 @@ const project = [
     href2: 'https://github.com/Wheezy049/dev-link',
   },
   {
+    title: 'Movie-App',
+    technologies: 'ReactJs, Tailwind',
+    img: '/movie.png',
+    href: 'https://movie-app-rose-beta.vercel.app/',
+    href2: 'https://github.com/Wheezy049/MovieApp',
+  },
+  {
     title: 'Fast-food',
     technologies: 'ReactJs, sass',
     img: '/fast-food.png',
@@ -37,13 +44,20 @@ const project = [
     href: 'https://dashboard-woad-one-36.vercel.app/',
     href2: 'https://github.com/Wheezy049/Dashboard',
   },
-  // {
-  //   title: 'Quiz App',
-  //   technologies: 'HTML/CSS, Javascript',
-  //   img: '/quiz.png',
-  //   href: 'https://quiz-app-five-kappa.vercel.app/',
-  //   href2: 'https://github.com/Wheezy049/Quiz-App',
-  // },
+  {
+    title: 'FinPay',
+    technologies: 'ReactJs, TypeScript, Tailwind',
+    img: '/finpay.png',
+    href: 'https://fastpay-olive.vercel.app/',
+    href2: 'https://github.com/Wheezy049/Fastpay',
+  },
+  {
+    title: 'Quiz App',
+    technologies: 'HTML/CSS, Javascript',
+    img: '/quiz.png',
+    href: 'https://quiz-app-five-kappa.vercel.app/',
+    href2: 'https://github.com/Wheezy049/Quiz-App',
+  },
   {
     title: 'Audiophile',
     technologies: 'HTML/CSS, JavaScript',
